3fdfe53259e7a930ff97cde1bd06125ff049fc28
eselbeus <eselbeus@gmail.com>
2019-01-30 22:42:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog 
-  def name 
+  def name
     @name
   end 
-  def name=()
-    name 
-    
+  def name=(name)
+    @name 
   end
-  
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed 
+    @breed
+  end
 end
